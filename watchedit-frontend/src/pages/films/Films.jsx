01fde49ddd9a49860d33838aeff5a9253b7f9043
TomF46 <t-d-f@live.co.uk>
2023-04-03 14:23:08 +0100

    Search on explore page

--- watchedit-frontend/src/pages/films/Films.jsx
+++ watchedit-frontend/src/pages/films/Films.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { toast } from "react-toastify";
-import { getFilmsPaginated } from "../../api/filmsApi";
+import debounce from 'lodash.debounce';
 import FilmGrid from "../../components/Films/FilmGrid";
 import PaginationControls from "../../components/PaginationControls";
 import { Link } from "react-router-dom";
+import { searchFilmsPaginated } from "../../api/filmsApi";
+import TextInput from "../../components/Inputs/TextInput";
 
 function Films({ isAdmin }) {
     const [films, setFilms] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
     const [page, setPage] = useState(1);
     const [filmsPerPage, setFilmsPerPage] = useState(20);
     const [isLastPage, setIsLastPage] = useState(false);
@@ -24,8 +27,17 @@ function Films({ isAdmin }) {
         if (lastPageLoaded != null) getFilms();
     }, [page]);
 
+    useEffect(() => {
+        let debounced = debounce(
+            () => { getFilms(); }, 50
+        );
+
+        debounced();
+    }, [searchTerm])
+
+
     function getFilms() {
-        getFilmsPaginated(page, filmsPerPage)
+        searchFilmsPaginated(searchTerm ,page, filmsPerPage)
             .then((res) => {
                 setFilms(res);
                 let lastPage = res.length != filmsPerPage;
@@ -48,6 +60,11 @@ function Films({ isAdmin }) {
     function handlePreviousPage() {
         var newPage = page - 1;
         setPage(newPage);
+    }
+
+    function handleSearchTermChange(event){
+        const { value } = event.target;
+        setSearchTerm(value);
     }
 
     return (
@@ -73,6 +90,24 @@ function Films({ isAdmin }) {
                 <p>Loading films....</p>
             ) : (
                 <div className="mt-4">
+                    <div className="search-controls bg-backgroundOffset mt-4 rounded-md">
+                        <div className="bg-primary rounded-t-md">
+                            <p className="text-white font-bold text-lg px-2 py-1">
+                                Search
+                            </p>
+                        </div>
+                        <div className="px-2 py-2">
+                            <div className="search-box">
+                                <TextInput
+                                    name="searchTerm"
+                                    label="Search"
+                                    value={searchTerm}
+                                    onChange={handleSearchTermChange}
+                                    required={false}
+                                />
+                            </div>
+                        </div>
+                    </div>
                     <FilmGrid films={films} editable={false} />
                     <PaginationControls
                         currentPage={page}
