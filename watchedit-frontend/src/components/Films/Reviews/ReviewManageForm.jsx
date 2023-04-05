c18c3f4ab0333d7f50202ddae208150b88d88c69
TomF46 <t-d-f@live.co.uk>
2023-04-05 11:52:18 +0100

    Buttons have more suitable icons where applicable.

--- watchedit-frontend/src/components/Films/Reviews/ReviewManageForm.jsx
+++ watchedit-frontend/src/components/Films/Reviews/ReviewManageForm.jsx
@@ -46,10 +46,10 @@ const ReviewManageForm = ({
                     disabled={saving}
                     className="bg-primary  text-white rounded py-2 px-4 hover:opacity-75 inline-flex items-center"
                 >
-                    <svg className="text-white h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    <svg className="text-white h-5 w-5" xmlns="http://www.w3.org/2000/svg" strokeWidth={2} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                     </svg>
-                    <span>{saving ? "Saving..." : "Save"}</span>
+                    <span className="ml-1">{saving ? "Saving..." : "Save"}</span>
                 </button>
             </div>
         </form>
