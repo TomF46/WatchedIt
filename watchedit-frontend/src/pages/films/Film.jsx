1f6319ab3fd8696b9129c2c0806f10318cfcfc9f
TomF46 <t-d-f@live.co.uk>
2023-04-12 11:19:35 +0100

    Improve film page Dates format, rating view

--- watchedit-frontend/src/pages/films/Film.jsx
+++ watchedit-frontend/src/pages/films/Film.jsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { confirmAlert } from 'react-confirm-alert';
+import { format, parseISO } from 'date-fns'
 import { getFilmById, removeFilm } from "../../api/filmsApi";
 import WatchedFilmControls from "../../components/Films/Watched/WatchedFilmControls";
 import FilmCreditsOverviewList from "../../components/Films/Credits/FilmCreditsOverviewList";
@@ -109,7 +110,7 @@ function Film({userIsAuthenticated, isAdmin}) {
                             <div className="grid grid-cols-12">
                                 <div className="col-span-12 md:col-span-8 bg-backgroundOffset p-4">
                                     <p>Name: {film.name}</p>
-                                    <p>Release date: {film.releaseDate}</p>
+                                    <p>Release date: {format(parseISO(film.releaseDate), "dd/MM/yyyy")}</p>
                                     <p>tagline: {film.shortDescription}</p>
                                     <p>Runtime: {film.runtime} minutes</p>
                                     <p>Categories:</p>
@@ -123,8 +124,25 @@ function Film({userIsAuthenticated, isAdmin}) {
                                     <p className="mt-4">Description: {film.fullDescription}</p>
                                 </div>
                                 <div className="col-span-12 md:col-span-4 text-center bg-backgroundOffset ml-2 p-4">
-                                    <h3>Rating</h3>
-                                    <p>{film.averageRating} / 10</p>
+                                    <h3 className="text-primary text-xl mb-4">Rating</h3>
+                                    {film.averageRating ? (
+                                        <div className="">
+                                            <p className="text-primary text-2xl">{film.averageRating} / 10</p>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 text-primary inline-flex items-center">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                            </svg>
+                                        </div>
+                                    ) : (
+                                        <>
+                                            <p>This film has not yet been rated.</p>
+                                            <Link
+                                                to={`/films/${id}/reviews/add`}
+                                                className="bg-primary text-white rounded py-2 px-4 hover:opacity-75 inline-block mt-2"
+                                            >
+                                                Add rating
+                                            </Link>
+                                        </>
+                                    )}
                                 </div>
                                 <div className="col-span-12">
                                     <div className="grid grid-cols-12">
