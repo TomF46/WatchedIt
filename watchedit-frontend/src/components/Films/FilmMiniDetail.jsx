bfe02e2902432ae38d6a61f2fc79e86a12dcd8df
TomF46 <t-d-f@live.co.uk>
2023-06-29 15:26:33 +0100

    Improve film mini detail to take up less height on larger screens.

--- watchedit-frontend/src/components/Films/FilmMiniDetail.jsx
+++ watchedit-frontend/src/components/Films/FilmMiniDetail.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 const FilmMiniDetail = ({ film }) => {
     return (
         <div className="grid grid-cols-12">
-           <div className="col-span-12 md:col-span-6 lg:col-span-4">
+           <div className="col-span-12 md:col-span-6 lg:col-span-6">
                 <div className="grid grid-cols-12 bg-backgroundOffset shadow rounded">
-                    <div className="col-span-6">
+                    <div className="col-span-6 md:col-span-3 lg:col-span-2">
                         <img src={film.posterUrl} className="h-full poster rounded-l" alt={`${film.name} poster.`} />
                     </div>
-                    <div className="col-span-6 p-2">
+                    <div className="col-span-6 md:col-span-9 lg:col-span-10 p-2">
                         <Link to={`/films/${film.id}`} className="text-primary font-bold hover:opacity-75">{film.name}</Link>
                         <p className="text-sm md:text-base">{film.shortDescription}</p>
                         {film.averageRating && (<p>{`${film.averageRating}/10`}</p>)}
