2a50382454b79a3d53e9735aa9bf091946358214
TomF46 <t-d-f@live.co.uk>
2024-01-12 13:52:56 +0000

    Dont send user notification if they comment on their own review.

--- WatchedIt.Api/Services/ReviewCommentsService/ReviewCommentsService.cs
+++ WatchedIt.Api/Services/ReviewCommentsService/ReviewCommentsService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using WatchedIt.Api.Models.CommentModels;
 using WatchedIt.Api.Services.Mapping;
 using WatchedIt.Api.Services.NotificationService;
@@ -22,9 +23,9 @@ namespace WatchedIt.Api.Services.ReviewCommentsService
         public async Task<PaginationResponse<GetReviewCommentDto>> GetCommentsForReview(int id, PaginationParameters parameters)
         {
             var review = await _context.Reviews.FirstOrDefaultAsync(r => r.Id == id);
-            if(review is null) throw new NotFoundException($"Review with Id '{id}' not found.");
-            
-        
+            if (review is null) throw new NotFoundException($"Review with Id '{id}' not found.");
+
+
             var query = _context.ReviewComments.Include(c => c.User).Include(c => c.Review).Where(x => x.Review.Id == id);
             var count = query.Count();
             query.OrderBy(x => x.CreatedDate);
@@ -36,12 +37,13 @@ namespace WatchedIt.Api.Services.ReviewCommentsService
         public async Task<GetReviewCommentDto> Add(int reviewId, int userId, AddCommentDto newComment)
         {
             var review = await _context.Reviews.Include(r => r.User).Include(r => r.Film).FirstOrDefaultAsync(r => r.Id == reviewId);
-            if(review is null) throw new NotFoundException($"Review with Id '{reviewId}' not found.");
+            if (review is null) throw new NotFoundException($"Review with Id '{reviewId}' not found.");
 
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
-            if(user is null) throw new BadRequestException($"User with Id '{userId}' does not exist");
+            if (user is null) throw new BadRequestException($"User with Id '{userId}' does not exist");
 
-            var comment = new ReviewComment {
+            var comment = new ReviewComment
+            {
                 Review = review,
                 User = user,
                 CreatedDate = DateTime.Now,
@@ -52,7 +54,8 @@ namespace WatchedIt.Api.Services.ReviewCommentsService
             await _context.ReviewComments.AddAsync(comment);
             await _context.SaveChangesAsync();
 
-            await _notificationService.SendNewCommentOnOwnedReviewNotification(review.User, comment);
+            //Only send notification if user isnt comment on own review.
+            if (review.User.Id != userId) await _notificationService.SendNewCommentOnOwnedReviewNotification(review.User, comment);
 
             return CommentMapper.MapReviewComment(comment);
         }
@@ -60,9 +63,9 @@ namespace WatchedIt.Api.Services.ReviewCommentsService
         public async Task<GetReviewCommentDto> Update(int commentId, int userId, UpdateCommentDto updatedComment)
         {
             var comment = await _context.ReviewComments.Include(c => c.User).Include(c => c.Review).FirstOrDefaultAsync(c => c.Id == commentId);
-            if(comment is null) throw new NotFoundException($"Comment with Id '{commentId}' not found.");
+            if (comment is null) throw new NotFoundException($"Comment with Id '{commentId}' not found.");
 
-            if(comment.User.Id != userId) throw new Exceptions.UnauthorizedAccessException($"User does not own this comment.");
+            if (comment.User.Id != userId) throw new Exceptions.UnauthorizedAccessException($"User does not own this comment.");
 
             comment.Text = updatedComment.Text;
             comment.UpdatedDate = DateTime.Now;
@@ -73,9 +76,9 @@ namespace WatchedIt.Api.Services.ReviewCommentsService
         public void Delete(int id, int userId)
         {
             var comment = _context.ReviewComments.Include(c => c.User).FirstOrDefault(c => c.Id == id);
-            if(comment is null) throw new NotFoundException($"Comment with Id '{id}' not found.");
+            if (comment is null) throw new NotFoundException($"Comment with Id '{id}' not found.");
 
-            if(comment.User.Id != userId) throw new Exceptions.UnauthorizedAccessException($"User does not own this comment.");
+            if (comment.User.Id != userId) throw new Exceptions.UnauthorizedAccessException($"User does not own this comment.");
 
             _context.ReviewComments.Remove(comment);
             _context.SaveChanges();
