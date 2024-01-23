7f468765377d5808f76e440150be6396430f9e7b
TomF46 <t-d-f@live.co.uk>
2024-01-23 13:17:15 +0000

    Bold -> Semibold

--- watchedit-frontend/src/pages/news/News.jsx
+++ watchedit-frontend/src/pages/news/News.jsx
@@ -67,24 +67,26 @@ function News() {
 
   return (
     <div className="news-page">
-      <h1 className="text-center text-primary text-4xl my-4 font-bold">News</h1>
+      <h1 className="text-center text-primary text-4xl my-4 font-semibold">
+        News
+      </h1>
       {userIsPublisher && (
         <div className="admin-controls bg-backgroundOffset mt-4 shadow rounded">
           <div className="bg-backgroundOffset2 rounded-t-md">
-            <p className="text-primary font-bold text-lg px-2 py-1">
+            <p className="text-primary font-semibold text-lg px-2 py-1">
               Publisher controls
             </p>
           </div>
           <div className="px-2 py-2">
             <Link
               to={"/news/add"}
-              className="bg-backgroundOffset2 text-primary font-bold rounded py-2 px-4 hover:opacity-75 inline-block"
+              className="bg-backgroundOffset2 text-primary font-semibold rounded py-2 px-4 hover:opacity-75 inline-block"
             >
               Add article
             </Link>
             <Link
               to={"/profile/news"}
-              className="bg-backgroundOffset2 text-primary font-bold rounded py-2 px-4 ml-2 hover:opacity-75 inline-block"
+              className="bg-backgroundOffset2 text-primary font-semibold rounded py-2 px-4 ml-2 hover:opacity-75 inline-block"
             >
               View my articles
             </Link>
@@ -97,7 +99,9 @@ function News() {
         <div className="mt-4">
           <div className="controls bg-backgroundOffset mt-4 rounded-md mb-4 shadow">
             <div className="bg-backgroundOffset2 rounded-t-md">
-              <p className="text-primary font-bold text-lg px-2 py-1">Search</p>
+              <p className="text-primary font-semibold text-lg px-2 py-1">
+                Search
+              </p>
             </div>
             <div className="px-2 py-2">
               <div className="search-box flex">
