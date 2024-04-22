e4f50930215db23107c87943a23d9a4a840ba3e2
TomF46 <t-d-f@live.co.uk>
2024-04-22 10:51:49 +0100

    Update permissions on where non admins can upload images.

--- WatchedIt.Api/Controllers/FilesController.cs
+++ WatchedIt.Api/Controllers/FilesController.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Util;
+
 using Microsoft.AspNetCore.Mvc;
+
 using WatchedIt.Api.Models.Files;
 using WatchedIt.Api.Services.File;
 using WatchedIt.Api.Services.Mapping;
@@ -25,7 +28,9 @@ namespace WatchedIt.Api.Controllers
         [HttpPost("upload")]
         public async Task<ActionResult<FileResponse>> UploadFileAsync(IFormFile file, string? prefix)
         {
-            if(!HttpContext.User.Identity.IsAuthenticated) return Unauthorized();
+            if (!HttpContext.User.Identity.IsAuthenticated) return Unauthorized();
+            var isAdmin = HttpContext.User.IsInRole("Administrator");
+            if (!isAdmin && prefix != "users") throw new Exceptions.UnauthorizedAccessException("User can't upload files to this location.");
             var userId = AuthMapper.MapLoggedInUserId(HttpContext);
             return Ok(await _fileService.Upload(userId, file, prefix));
         }
